2b3808cac2578ca13dabbb9d28c8646261c73d3f
PescaMA <pescariu@real.cpp>
2024-03-06 12:17:31 +0200

    invented overcomplicated dfs of 40 lines...

--- main.cpp
+++ main.cpp
@@ -1,19 +1,46 @@
 #include <iostream>
 #include <fstream>
-#include <map>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 #include <algorithm>
+#include <stack>
 
-class DFA
+class FA
 {
-    public:
+protected:
     int n,m; /// numarul de stari, respectiv tranzitii
     std::vector<int> states;
     int initialState;
-    std::map<int,bool> finalStates;
-    std::map<std::pair<int,char>,int> tranz; /// tranzitii
-    public:
-    DFA():n(0){}
+    std::unordered_set<int> finalStates;
+    std::unordered_map<int, std::unordered_map<char,std::vector<int> > > tranz; /// tranzitii
+
+    void runTests(std::istream& in,std::ostream& out)
+    { /// fara accepted() implementat, functia asta nu are sens momentan.
+        /// insa in clasele derivate, o sa o fac publica fara a o rescrie.
+        int testCount;
+        in >> testCount;
+        for(int i=0;i<testCount;i++)
+        {
+            std::string word;
+            in >> word;
+            if(accepted(word))
+            {
+                out << "DA\n";
+                std::cout << "Drumul este: ";
+                path(word);
+                std::cout << '\n';
+            }
+            else
+            {
+                out << "NU\n";
+                std::cout << "Cuvantul nu e acceptat\n";
+            }
+
+        }
+    }
+public:
+    FA():n(0){}
     void read(std::istream& in)
     {
         in >> n;
@@ -29,7 +56,7 @@ class DFA
             int startNode, endNode; /// starea de plecare si terminare a tranzitiei (muchiei)
             char letter; /// simbolul din limbaj.
             in >> startNode >> endNode >> letter;
-            tranz[std::pair<int,char>(startNode,letter)] = endNode;
+            tranz[startNode][letter].push_back(endNode);
         }
         in >>initialState;
         std::string listFinals;
@@ -39,48 +66,157 @@ class DFA
         {
             int state;
             in >> state;
-            finalStates[state] = true;
+            finalStates.insert(state);
         }
     }
+    virtual bool accepted(std::string word) = 0;
+    virtual void path(std::string word) = 0;/// assumes word is accepted and displays path.
+    friend std::istream& operator>>(std::istream& in, FA& myDFA);
+};
+std::istream& operator>>(std::istream& in, FA& myFA)
+{
+    myFA.read(in);
+    return in;
+}
+
+
+class DFA : public FA
+{
+public:
+    using FA::runTests;
     bool accepted(std::string word)
     {
         int node = initialState;
-        for(int i = 0; i < word.size(); i++)
+        for(unsigned int i = 0; i < word.size(); i++)
         {
-            std:: pair<int,char> t(node,word[i]);
-            if(tranz.find(t) == tranz.end())
+            if(tranz[node].find(word[i]) == tranz[node].end())
                 return false;
-            node = tranz[t];
+            node = tranz[node][word[i]][0]; /// ar trebui sa existe doar o muchie pt litera curenta.
         }
         return (finalStates.find(node) != finalStates.end());
     }
-    void runTests(std::ifstream& in,std::ofstream& out)
+    void path(std::string word)
     {
-        int testCount;
-        in >> testCount;
-        for(int i=0;i<testCount;i++)
+        int node = initialState;
+        std::cout << node;
+        for(unsigned int i = 0; i < word.size(); i++)
         {
-            std::string word;
-            in >> word;
-            if(accepted(word))
-                out << "DA\n";
-            else
-                out << "NU\n";
+            node = tranz[node][word[i]][0];
+            std::cout << "->" << node;
         }
     }
-    friend std::istream& operator>>(std::istream& in, DFA& myDFA);
 };
-std::istream& operator>>(std::istream& in, DFA& myDFA)
+class NFA : public FA
 {
-    myDFA.read(in);
-    return in;
-}
+public:
+    using FA::runTests;
+
+    /// cel mai complicat DFS ever. N-am mai vazut pe net DFS care are
+    /// stack de iteratori (ca sa aiba maxim O(n) memorie, nu O(n^2) pe un graf complet?
+    bool accepted(std::string word){
+        if(word.size() == 0)
+            return (finalStates.find(initialState) != finalStates.end());
+        if(tranz[initialState].find(word[0]) == tranz[initialState].end())
+            return false;
+        std::stack<int> prevNodes;
+        prevNodes.push(initialState);
+        std::stack< std::vector<int>::iterator > its;
+        its.push(tranz[initialState][word[0]].begin());
+        for(int i=1;i >= 0; i++)
+        {
+            if(its.empty())return false;
+            if(its.top() == tranz[prevNodes.top()][word[i-1]].end())
+            {
+                i-=2;
+                its.pop();
+                prevNodes.pop();
+                if(!its.empty())
+                    its.top()++;
+                continue;
+            }
+            int node = *(its.top());
+
+            if(i == word.size()-1)
+            {
+                for(const auto& lastNode:tranz[node][word[i]])
+                    if (finalStates.find(lastNode) != finalStates.end())
+                        return true;
+                i-=1;
+                its.top()++;
+                continue;
+            }
+            if(tranz[node].find(word[i]) == tranz[node].end())
+            {
+                i-=1;
+                its.top()++;
+                continue;
+            }
+            prevNodes.push(node);
+            its.push(tranz[node][word[i]].begin());
+        }
+        return false;
+    }
+    void path(std::string word)    {
+
+    }
+    /*
+    bool acceptedDFS_recursiv(std::string word, int node,int i=0)
+    {
+        if(i == word.size())
+            return (finalStates.find(node) != finalStates.end());
+
+        if(tranz[node].find(word[i]) != tranz[node].end())
+            for(const auto& nextNode:tranz[node][word[i]])
+                if(acceptedDFS(word,nextNode,i+1))
+                    return true;
+        return false;
+    }
+
+    stack<int> pathDFS(std::string word, int node, int i=0)
+    {
+        if(i == word.size())
+            if (finalStates.find(node) != finalStates.end())
+            {
+                stack<int> result =
+            }
+
+        if(tranz[node].find(word[i]) != tranz[node].end())
+            for(const auto& nextNode:tranz[node][word[i]])
+                if(acceptedDFS(word,nextNode,i+1))
+                    return true;
+        return false;
+    }*/
+
+    /// merge, dar e mai greu sa construiesti drumul.
+    bool acceptedBFS(std::string word)
+    {
+        std::unordered_set<int> nodes;
+        nodes.insert(initialState);
+        for(unsigned int i = 0; i < word.size(); i++)
+        {
+            std::unordered_set<int> oldNodes = nodes;
+            nodes.clear();
+            for(const auto& node:oldNodes)
+            {
+                if(tranz[node].find(word[i]) != tranz[node].end())
+                    for(const auto& nextNode:tranz[node][word[i]])
+                        nodes.insert(nextNode);
+            }
+            if(nodes.empty()) return false;
+        }
+        for (const auto& node: nodes)
+            if(finalStates.find(node) != finalStates.end())
+                return true;
+        return false;
+    }
+};
+
 
 int main()
 {
     std::ifstream fin("input.txt");
     std::ofstream fout("output.txt");
-    DFA automata;
+    NFA automata;
     fin >> automata;
     automata.runTests(fin,fout);
 }
