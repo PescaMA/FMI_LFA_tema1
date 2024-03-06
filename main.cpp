ae49d8b4ecb6b2fd7a4310fe37e8858df2d5eef0
PescaMA <pescariu@real.cpp>
2024-03-07 00:13:08 +0200

    lambda-nfa without path...

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <algorithm>
 #include <stack>
+#include <deque>
 
 class FA
 {
@@ -84,8 +85,16 @@ class DFA : public FA
 {
 public:
     using FA::runTests;
-    bool accepted(std::string word)
+    bool accepted(std::string word){
+        return accepted(word,false);
+    }
+    void path(std::string word){
+        accepted(word,true);
+    }
+    bool accepted(std::string word,bool outputPath = false)
     {
+        if(word.size() == 0)
+            return (finalStates.find(initialState) != finalStates.end());
         int node = initialState;
         for(unsigned int i = 0; i < word.size(); i++)
         {
@@ -95,52 +104,60 @@ public:
         }
         return (finalStates.find(node) != finalStates.end());
     }
-    void path(std::string word)
-    {
-        int node = initialState;
-        std::cout << node;
-        for(unsigned int i = 0; i < word.size(); i++)
-        {
-            node = tranz[node][word[i]][0];
-            std::cout << "->" << node;
-        }
-    }
 };
 class NFA : public FA
 {
 public:
     using FA::runTests;
+    bool accepted(std::string word){return acceptedLONG(word,false);}
+    void path(std::string word)    {
+        acceptedLONG(word,true);
+    }
+    void printPath(std::deque<int> nodes, int currentNode, int lastNode)
+    {
+        while(!nodes.empty())
+        {
+            std:: cout << nodes.front() << ' ';
+            nodes.pop_front();
+        }
+        std::cout << currentNode << ' ' << lastNode;
+    }
+
 
     /// cel mai complicat DFS ever. N-am mai vazut pe net DFS care are
     /// stack de iteratori (ca sa aiba maxim O(n) memorie, nu O(n^2) pe un graf complet?
-    bool accepted(std::string word){
+    bool acceptedLONG(std::string word,bool outputPath = false)
+    {
         if(word.size() == 0)
             return (finalStates.find(initialState) != finalStates.end());
         if(tranz[initialState].find(word[0]) == tranz[initialState].end())
             return false;
-        std::stack<int> prevNodes;
-        prevNodes.push(initialState);
+        std::deque<int> prevNodes;
+        prevNodes.push_back(initialState);
         std::stack< std::vector<int>::iterator > its;
         its.push(tranz[initialState][word[0]].begin());
         for(int i=1;i >= 0; i++)
         {
             if(its.empty())return false;
-            if(its.top() == tranz[prevNodes.top()][word[i-1]].end())
+            if(its.top() == tranz[prevNodes.back()][word[i-1]].end())
             {
                 i-=2;
                 its.pop();
-                prevNodes.pop();
+                prevNodes.pop_back();
                 if(!its.empty())
                     its.top()++;
                 continue;
             }
             int node = *(its.top());
-
             if(i == word.size()-1)
             {
                 for(const auto& lastNode:tranz[node][word[i]])
                     if (finalStates.find(lastNode) != finalStates.end())
+                    {
+                        if(outputPath)
+                            printPath(prevNodes,node,lastNode);
                         return true;
+                    }
                 i-=1;
                 its.top()++;
                 continue;
@@ -151,13 +168,10 @@ public:
                 its.top()++;
                 continue;
             }
-            prevNodes.push(node);
+            prevNodes.push_back(node);
             its.push(tranz[node][word[i]].begin());
         }
         return false;
-    }
-    void path(std::string word)    {
-
     }
     /*
     bool acceptedDFS_recursiv(std::string word, int node,int i=0)
@@ -190,6 +204,8 @@ public:
     /// merge, dar e mai greu sa construiesti drumul.
     bool acceptedBFS(std::string word)
     {
+        if(word.size() == 0)
+            return (finalStates.find(initialState) != finalStates.end());
         std::unordered_set<int> nodes;
         nodes.insert(initialState);
         for(unsigned int i = 0; i < word.size(); i++)
@@ -210,13 +226,78 @@ public:
         return false;
     }
 };
+class l_NFA : public FA
+{
+public:
+    using FA::runTests;
+    bool accepted(std::string word){return acceptedLONG(word,false);}
+    void path(std::string word)    {
+        acceptedLONG(word,true);
+    }
+    bool acceptEmpty()
+    {
+        if(finalStates.find(initialState) != finalStates.end())
+            return true;
+        for(const auto& lambdaNode:tranz[initialState][0])
+            if(finalStates.find(lambdaNode) != finalStates.end())
+                return true;
+        return false;
+    }
+    bool acceptedLONG(std::string word, bool outputPath = false)
+    {
+        if(word.empty())
+            return acceptEmpty();
+        std::stack<std::pair<int,int> > stk;
+        stk.push({initialState,0});
 
+        while(!stk.empty())
+        {
+            std::unordered_set<int> same,viz;
+            int node = stk.top().first;
+            int i = stk.top().second;
+            stk.pop();
+
+            if(i == word.size())
+            {
+                if(finalStates.find(node) != finalStates.end())
+                    return true;
+                continue;
+            }
+
+            std::stack<int> startNodes;
+            startNodes.push(node);
+            same.insert(node);
+            /// lambda tranzitions
+            while(!startNodes.empty())
+            {
+                int sameNode = startNodes.top();
+                startNodes.pop();
+                for (const auto& nextNode:tranz[node]['0'])
+                {
+                    if(same.find(nextNode) == same.end())
+                    {
+
+                        same.insert(nextNode);
+                        startNodes.push(nextNode);
+                    }
+                }
+                /// normal tranzitions
+                for (const auto& nextNode:tranz[sameNode][word[i]])
+                    viz.insert(nextNode);
+            }
+            /// the unique nodes
+            for (const auto& nextNode:viz)
+                stk.push({nextNode,i+1});
+        }
+        return false;
+    }
+};
 
 int main()
 {
     std::ifstream fin("input.txt");
     std::ofstream fout("output.txt");
-    NFA automata;
+    l_NFA automata;
     fin >> automata;
     automata.runTests(fin,fout);
 }
