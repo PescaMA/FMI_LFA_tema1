d8a2ed5909567dbae81c6ee2b123ba3c7a0ea60d
PescaMA <pescariu@real.cpp>
2024-03-11 11:43:47 +0200

    working on refactoring

--- main.cpp
+++ main.cpp
@@ -1,47 +1,41 @@
-/// un cod FOARTE LUNG (asa se intampla cand codez eu clase + am incercat sa fie citibil +
-/// probabil m-am complicat enorm) care respecta toate cerintele si individualizate,
+/// un cod FOARTE LUNG care respecta toate cerintele si individualizate,
 /// implementand algoritmi relativ eficienti pe DFA, NFA, lambda-NFA.
 /// (facut de Pescariu Matei-Alexandru - tema 1 LFA).
-/// P.S. sper ca nu e vreo problema ca am pus comentariile in engleza ca sa fie mai universal, sa il pot pune usor pe GitHub.
 #include <iostream>
 #include <fstream>
 #include <unordered_map>
 #include <unordered_set>
 #include <stack>
 #include <deque>
+#include <vector>
 
-class FA
-{ /// abstract implementation of a finite automata
+class FA{ /// abstract implementation of a finite automata
 protected:
-    const char LAMBDA = '@';
+    const char LAMBDA = '@'; /// the character decided to be lambda/epsilon/empty symbol.
     int startState;
     std::unordered_set<int> finalStates; /// unordered_set for O(1) verification if a state is final.
     std::unordered_map<int, std::unordered_map<char,std::unordered_set<int> > > edges;
     /// edges is an adjacency list. [StartingNode] [Edge "cost"/letter] [EndingNode].
     /// Why so many unordered? For theoretical O(1) access. When using the list I go on transitions based on the
     /// letter more than based on the next node, so it comes before for faster access.
-    /// The set is used for lambdaNFAs to search for the lambda transition. It also is compatible with DFA/NFA.
+    /// The set<int> is used for lambdaNFAs to search for the lambda transition. It also is compatible with DFA/NFA.
 
-    virtual void path(std::string word) {};/// displays path in console. MUST be implemented in derived classes.
-    void runTests(std::istream& in,std::ostream& out)
-    { /// without the implementation of accepted() and path() it doesn't work.
+    virtual void path(std::string word) {}/// displays path in console. MUST be implemented in derived classes.
+    void runTests(std::istream& in,std::ostream& out){ /// without the implementation of accepted() and path() it doesn't work.
     /// Therefore, it only becomes public in derived classes.
         int testCount;
         in >> testCount;
-        for(int i=0;i<testCount;i++)
-        {
+        for(int i=0;i<testCount;i++){
             std::string word;
             in >> word;
             if(word == "_") word = ""; /// Empty word is considered to be the underscore.
-            if(accepted(word))
-            {
+            if(accepted(word)){
                 out << "DA\n"; /// means Yes (the word is accepted)
                 std::cout << "Drumul este: "; /// means "The path is: "
                 path(word);
                 std::cout << '\n';
             }
-            else
-            {
+            else{
                 out << "NU\n"; /// means No
                 std::cout << "Cuvantul nu e acceptat.\n"; /// means "The word isn't accepted."
             }
@@ -51,41 +45,35 @@ public:
     FA(){}
     FA(const FA& fa){set_FA(fa);}
     void set_FA(FA newFa){startState = newFa.startState; finalStates = newFa.finalStates; edges = newFa.edges;}
-    FA get_FA(){
+    FA get_FA(){ /// gets base common variables (needed in derived classes).
         FA result;
         result.startState = startState;
         result.finalStates = finalStates;
         result.edges = edges;
         return result;
     }
-    void read(std::istream& in)
-    {/// input data format is present in requirements.txt
-
+    void read(std::istream& in){/// input data format is present in requirements.txt
         int n,m;
         std::deque<int> states;
+        std::string listFinals;
+        int nrFinalStates;
         /// Didn't use these variables so I didn't put them as members.
-
         in >> n;
-        for(int i=0;i<n;i++)
-        {
+        for(int i=0;i<n;i++){
             int state;
             in >> state;
             states.push_back(state);
         }
         in >> m;
-        for(int i=0;i<m;i++)
-        {
+        for(int i=0;i<m;i++){
             int startNode, endNode;
             char letter;
             in >> startNode >> endNode >> letter;
             edges[startNode][letter].insert(endNode);
         }
         in >>startState;
-        std::string listFinals;
-        int nrFinalStates;
         in >> nrFinalStates;
-        for(int i=0;i<nrFinalStates;i++)
-        {
+        for(int i=0;i<nrFinalStates;i++){
             int state;
             in >> state;
             finalStates.insert(state);
@@ -94,27 +82,22 @@ public:
     virtual bool accepted(std::string word, bool a = false){return false;} /// MUST be implemented in non-abstract FA.
     friend std::istream& operator>>(std::istream& in, FA& myDFA);
 };
-std::istream& operator>>(std::istream& in, FA& myFA)
-{
+std::istream& operator>>(std::istream& in, FA& myFA){
     myFA.read(in);
-    return in;
+    return in; /// used read so I could write that inside the class.
 }
 
-
-class DFA : public FA
-{
+class DFA : public FA{
 public:
     using FA::runTests; /// to make the runTests public.
     void path(std::string word){
         accepted(word,true);
     }
-    bool accepted(std::string word,bool outputPath = false)
-    { /// simple acceptor in O(L) where L is the length of the word.
+    bool accepted(std::string word,bool outputPath = false){ /// simple acceptor in O(L) where L is the length of the word.
         if(word.size() == 0)
             return (finalStates.find(startState) != finalStates.end());
         int node = startState;
-        for(unsigned int i = 0; i < word.size(); i++)
-        {
+        for(unsigned int i = 0; i < word.size(); i++){
             if(edges[node].find(word[i]) == edges[node].end())
                 return false;
             node = *(edges[node][word[i]].begin()); /// since it's a DFA we should only have 1 transition per letter per node.
@@ -131,182 +114,112 @@ public:
     void path(std::string word){
         accepted(word,true);
     }
-    void printPath(std::deque<int> nodes, int lastNode)
-    {
-        while(!nodes.empty())
-        {
-            std:: cout << nodes.front() << ' ';
-            nodes.pop_front();
-        }
-        std::cout << lastNode;
+    void printPath(const std::vector<int>& nodes, std::string word){
+        for(int unsigned i = 0;i < word.size(); i++)
+            std:: cout << nodes[i] << " -" << word[i] << "-> ";
+        std::cout << nodes[word.size()];
     }
-
-
-    /// Slightly OVERCOMPLICATED (took some time to debug). works but not traditional DFS.
-    /// It was the first solution to also output the path that I thought of.
-    /// Uses a stack of iterators, which ended up needing the last node
-    /// to verify that the end of the iterator isn't reached (luckily
-    /// prevNodes is also used in outputing the result).
-
-    /// It doesn't use a normal stack because I thought it would be too memory
-    /// inefficient to have worst case of n states per every l letter stored instead of l iterators.
-    /// At lambda-NFA I gave up on this design in favor of simplicity.
-    bool accepted(std::string word,bool outputPath = false)
-    {
-        if(word.size() == 0) /// empty word (lambda)
-            return (finalStates.find(startState) != finalStates.end());
-        if(edges[startState].find(word[0]) == edges[startState].end())
-            return false; /// if the first letter doesn't lead anywhere
-
-        std::deque<int> prevNodes;
-        prevNodes.push_back(startState);
-
-        std::stack< std::unordered_set<int>::iterator > its;
-        its.push(edges[startState][word[0]].begin());
-
-        /// this for loop is just weird. i is the index of the place in the word
-        /// and when going on a bad path we decrease it once (i-=2;i++). So it
-        /// has to end when running out of every path (backtracking to 0th letter).
-        for(int i=1;i >= 0; i++)
-        {
-            if(its.empty())return false;
-            if(its.top() == edges[prevNodes.back()][word[i-1]].end())
-            {/// if we reached the end of the current iterator.
-                i-=2;
-                its.pop();
-                prevNodes.pop_back();
-                if(!its.empty())
-                    its.top()++;
-                continue;
-            }
-            int node = *(its.top());
-
-            if((unsigned)i == word.size())
-            { /// reached the end of the word; testing if finalState.
-                if (finalStates.find(node) != finalStates.end())
-                {
-                    if(outputPath)
-                        printPath(prevNodes,node);
-                    return true;
-                }
-                i-=1;
-                its.top()++;
-                continue;
-            }
-
-
-            if(edges[node].find(word[i]) == edges[node].end())
-            {/// testing if transition doesn't exists
-                i-=1;
-                its.top()++;
-                continue;
-            }
-            prevNodes.push_back(node);
-            its.push(edges[node][word[i]].begin());
-        }
-        return false;
+    bool acceptedEmpty(bool outputPath = false){ /// handles empty word
+        if(finalStates.find(startState) == finalStates.end())
+            return false;
+        if(outputPath)
+            std::cout << startState;
+        return true;
     }
+    bool accepted(std::string word, bool outputPath = false){
+        if(word.empty()) /// special case
+            return acceptedEmpty(outputPath);
 
-    /// classic recursive DFS. Didn't know how to show path so I codded the overcomplicated one.
-    bool acceptedDFS(std::string word, int node,int i=0){
-        if((unsigned)i == word.size())
-            return (finalStates.find(node) != finalStates.end());
+        std::unordered_set<int> visited[word.size() + 2]; /// which nodes have been visited for each letter to
+        /// not compute the same thing multiple times. Costs O(word.size * nr_states) space worst case.
 
-        if(edges[node].find(word[i]) != edges[node].end())
-            for(const auto& nextNode:edges[node][word[i]])
-                if(acceptedDFS(word,nextNode,i+1))
-                    return true;
-        return false;
-    }
+        std::vector<int> solutionNodes(word.size()+2);
+        std::stack<std::pair<int,int> > stk; /// remembers the nodes and the index of the added nodes
+        /// to the iterative depth first search "simulated" stack
+        stk.push({startState,0});
 
-    /// first coded. Doesn't output path (can't remember where all paths came from).
-    bool acceptedBFS(std::string word){
-        if(word.size() == 0)
-            return (finalStates.find(startState) != finalStates.end());
-        std::unordered_set<int> nodes;
-        nodes.insert(startState);
-        for(unsigned int i = 0; i < word.size(); i++){
-            std::unordered_set<int> oldNodes = nodes;
-            nodes.clear();
-            for(const auto& node:oldNodes){
-                if(edges[node].find(word[i]) != edges[node].end())
-                    for(const auto& nextNode:edges[node][word[i]])
-                        nodes.insert(nextNode);
-            }
-            if(nodes.empty()) return false;
-        }
-        for (const auto& node: nodes)
-            if(finalStates.find(node) != finalStates.end())
+        while(!stk.empty()){
+            int node = stk.top().first;
+            unsigned int i = stk.top().second;
+            stk.pop();
+
+            solutionNodes[i] = node; /// position i will be rewritten if dfs backtracks.
+
+            if(visited[i].find(node) != visited[i].end())
+                continue;
+            visited[i].insert(node);
+
+            if(i == word.size()){/// reached the end of the word. Finish or backtrack
+                if(finalStates.find(node) == finalStates.end())
+                    continue;
+                if(outputPath)
+                    printPath(solutionNodes,word);
                 return true;
+            }
+
+            for (const auto& nextNode:edges[node][word[i]])
+                stk.push({nextNode,i+1});
+        }
         return false;
     }
-    DFA to_DFA();
 };
 
-/// the lambda non deterministic finite automata. The most important one, containg all NFAs or DFAs.
-class l_NFA : public FA
-{
+/// the lambda non deterministic finite automata. The most important one, NFAs and DFAs being special cases of this.
+class l_NFA : public FA{
 protected:
-    void path(std::string word)    {
-        accepted(word,true);
-    }
-    bool checkTransition(int prevNode,char c,int nextNode){
+    void path(std::string word){ accepted(word,true); }
+    bool checkTransition(int prevNode,char c,int nextNode){ /// checks if transition exists.
         return edges.find(prevNode) != edges.end() &&
                edges[prevNode].find(c) != edges[prevNode].end() &&
                edges[prevNode][c].find(nextNode) != edges[prevNode][c].end();
     }
-    int getLambdaLast(int prevNode,char c,int nextNode){
+    int getLambdaLast(int prevNode,char c,int nextNode){ /// gets a lambda reachable node that has a transition c to nextNode.
         std::unordered_set<int> same = getReachableLambda(prevNode);
-        for(const auto& sameNode:same)
-        {
+        for(const auto& sameNode:same){
             if(!checkTransition(sameNode,c,nextNode))
                 continue;
             return sameNode;
         }
         return prevNode;
     }
-    std::unordered_map<int,int> getLambdaPrevious(int startNode){
-        std::unordered_map<int,int> lambdaPath; /// has 2 uses: remembers what
-        /// nodes have been visited (to ignore lambda cycles) and remembers from
-        /// which node we got to the current node.
+    std::unordered_map<int,int> getLambdaPrevious(int startNode){ /// allows computation of lambda paths by saying which
+        /// node we reached the current node from.
+
+        std::unordered_map<int,int> lambdaPath; /// has 2 uses: remembers what nodes have been visited (to ignore
+        ///lambda cycles) and remembers from which node we got to the current node.
         lambdaPath[startNode] = startNode; /// to figure out it's the first one.
 
         std::stack<int> stk;
         stk.push(startNode);
-        while(!stk.empty())
-        {
+        while(!stk.empty()){
             int node = stk.top();
             stk.pop();
 
-            for (const auto& sameNode:edges[node][LAMBDA])
-            { /// sameNode because we can get there through lambda transitions in the same step.
+            for (const auto& sameNode:edges[node][LAMBDA]){ /// sameNode because we can get there through lambda transitions in the same step.
                 if(lambdaPath.find(sameNode) != lambdaPath.end())
                     continue; /// if already visited we ignore it.
                 stk.push(sameNode); /// we'll have to check it.
-                lambdaPath[sameNode] =  node;
+                lambdaPath[sameNode] =  node; /// reached it from the node called node.
             }
         }
         return lambdaPath;
     }
     std::deque<int> getLambdaPath(int startNode,int endNode){
-        if(startNode == endNode)
-            return {};
+        if(startNode == endNode) return {};
         std::unordered_map<int,int> lambdaPath = getLambdaPrevious(startNode);
-        /// backtracks to the beginning node, saving the nodes.
 
         std::deque<int> result;
-        while(endNode != lambdaPath[endNode])
-        {
+        while(endNode != lambdaPath[endNode]){ /// backtracks path from end to start.
             result.push_front(endNode);
             endNode = lambdaPath[endNode];
         }
         return result;
     }
+
     /// helper function to get the lambda path between 2 nodes (used in printing path).
     std::deque<int> getLambdaTransitionPath(int prevNode, char c, int nextNode){
 
         int lastNode = getLambdaLast(prevNode,c,nextNode);
-
         if(!checkTransition(lastNode,c,nextNode))
             return {}; /// control case where no path was found
 
@@ -403,7 +316,7 @@ protected:
 public:
     using FA::runTests;
     bool accepted(std::string word, bool outputPath = false){
-        ///std::unordered_set<int> visited[word.size() + 2]; // NEW
+        std::unordered_set<int> visited[word.size() + 2];
 
         if(word.empty()) /// special case
             return acceptedEmpty(outputPath);
@@ -424,9 +337,9 @@ public:
             stk.pop();
             ///std::cout <<"nod/i: " <<  node << ' ' << i << '\n';
 
-            ///  if(visited[i].find(node) != visited[i].end()) // NEW
-            ///    continue; // NEW
-            ///visited[i].insert(node);
+            if(visited[i].find(node) != visited[i].end())
+                continue;
+            visited[i].insert(node);
 
             if(i == word.size())
             {/// reached the end of the word. Finish or backtrack
@@ -441,7 +354,7 @@ public:
             }
 
             std::unordered_set<int> same,vis; /// "same" is for nodes reachable with a lambda path
-            /// and "vis" for nodes that can be visited on the next
+            /// and "vis" for nodes that CAN be visited on the next
             same = getReachableLambda(node);
 
             for( const auto& sameNode:same)
@@ -455,9 +368,7 @@ public:
         return false;
     }
     NFA to_NFA();
-    DFA to_DFA();
 };
-
 NFA l_NFA::to_NFA()
 {
     FA original;
@@ -469,27 +380,19 @@ NFA l_NFA::to_NFA()
 
     for(const auto& node:nodes)
     {
-        if(edges.find(node) == edges.end())continue;
+        std::unordered_set<int> reachable = getReachableLambda(node);
+        edges[node][LAMBDA].clear();
 
-        if(edges[node].find(LAMBDA) == edges[node].end())
-            continue;
-        if(edges[node][LAMBDA].empty()) continue;
-
-        edges[node][LAMBDA].erase(node);
-
-
-        while(!edges[node][LAMBDA].empty())
-        { /// we can't use a for because the iterated vector might change.
-            int nextNode = *(edges[node][LAMBDA].begin());
-            if(finalStates.find(nextNode) != finalStates.end())
+        for(const auto& sameNode:reachable){
+            if(finalStates.find(sameNode) != finalStates.end())
                 finalStates.insert(node);
-            for(const auto& edge:edges[nextNode])
+            for(const auto& edge:edges[sameNode])
             {
+                if(edge.first == LAMBDA)
+                    continue;
                 for(const auto& newNode:edge.second)
                     edges[node][edge.first].insert(newNode);
             }
-            edges[node][LAMBDA].erase(nextNode);
-            edges[node][LAMBDA].erase(node);
         }
     }
 
@@ -504,6 +407,6 @@ int main()
     std::ofstream fout("output.txt");
     l_NFA automata;
     fin >> automata;
-    ///NFA convNFA = automata.to_NFA();
-    automata.runTests(fin,fout);
+    NFA convNFA = automata.to_NFA();
+    convNFA.runTests(fin,fout);
 }
